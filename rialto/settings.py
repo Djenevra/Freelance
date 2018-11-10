5c6ced7fad2ec6b8caabb4866d13243e97e2c928
Djenevra <begimayapsamatova@gmail.com>
2018-11-10 13:37:00 +0600

    Check bazar

--- rialto/settings.py
+++ rialto/settings.py
@@ -83,9 +83,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rialto.wsgi.application'
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 DATABASES = {
     'default': {
@@ -120,3 +118,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
